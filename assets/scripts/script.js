b7de34570000d872bce15d83c4a46c10f4cf4844
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2021-09-26 14:28:48 +0200

    Music button working

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,22 @@
+const muzak = new Audio("assets/music/the-show-no-poem.mp3")
+
+$("#music-button").on("click", playMusic)
+
 function playMusic(){
-    alert("Music!")
+
+    console.log("play")
+    muzak.play();
+  muzak.volume = 0.5;
+  muzak.loop = true;
+  muzak.muted = false;
+  $("#music-button").on("click", checkMute)
+  
+}
+
+function checkMute(){
+    if (muzak.volume === 0.5) {
+        muzak.volume = 0;
+      } else if (muzak.volume === 0) {
+        muzak.volume = 0.5;
+      }
 }
