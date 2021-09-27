fbecc827d9d87b4e3cc520161fe4e93cc8177076
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2021-09-27 13:42:36 +0200

    Media queries for top return button

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,34 +1,33 @@
 const muzak = new Audio("assets/music/the-show-no-poem.mp3")
 
-const toggleButton = document.getElementsByClassName("toggle-button")[0]
-const toggleClass = document.getElementsByClassName("nav-li-flex-parent")[0]
-
-toggleButton.addEventListener("click", ()=>{
-
-
- toggleClass.classList.toggle("active");
-
+$(".toggle-button").on("click", () => {
+  $(".nav-li-flex-parent").slideToggle()
 })
-
 
 $("#music-button").on("click", playMusic)
 
 function playMusic(){
 
-  console.log("play")
   muzak.play();
   muzak.volume = 0.5;
   muzak.loop = true;
   muzak.muted = false;
-  $("#music-button").on("click", checkMute);
+
+  $("#music-button").on("click", () => {
+    if (muzak.volume === 0.5) {
+      muzak.volume = 0;
+    } else if (muzak.volume === 0) {
+      muzak.volume = 0.5;
+    }
+  });
 
 }
 
 
-function checkMute(){
-  if (muzak.volume === 0.5) {
-    muzak.volume = 0;
-  } else if (muzak.volume === 0) {
-    muzak.volume = 0.5;
-  }
-}
+// function checkMute(){
+//   if (muzak.volume === 0.5) {
+//     muzak.volume = 0;
+//   } else if (muzak.volume === 0) {
+//     muzak.volume = 0.5;
+//   }
+// }
