40f085be3297794b24569f55d528784da7067249
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2021-09-27 16:31:16 +0200

    Gsap working

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,16 @@
+// wait until DOM is ready
+$(document).ready(function() {
+  
+  // wait until images, links, fonts, stylesheets, scripts, and other media assets are loaded
+  $(window).bind("load", function() {
+    
+    // GSAP custom code goes here
+    gsap.from("#top", {opacity: 0, x: "-50vw", duration: 2})
+  });
+  
+  
+});
+
 const muzak = new Audio("assets/music/the-green-door.mp3")
 
 $(".toggle-button").on("click", () => {
