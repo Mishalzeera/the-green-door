b517c993deafa0fd8983b6c602c11bb40013de67
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2021-09-28 11:21:23 +0200

    Carousel code attempts

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,21 +2,26 @@
 $(document).ready(function() {
   
   // wait until images, links, fonts, stylesheets, scripts, and other media assets are loaded
+
   $(window).bind("load", function() {
-    
-    // GSAP custom code goes here
-    gsap.from("#top", {opacity: 0, x: "-50vw", duration: 2})
+   headingAnim()
+    imgCarousel()
   });
   
   
 });
 
-const muzak = new Audio("assets/music/the-green-door.mp3")
+
+
+// mobile icon nav toggle
 
 $(".toggle-button").on("click", () => {
   $(".nav-li-flex-parent").slideToggle()
 })
 
+// Music logic 
+
+const muzak = new Audio("assets/music/the-green-door.mp3")
 $("#music-button").on("click", playMusic)
 
 function playMusic(){
@@ -36,4 +41,31 @@ function playMusic(){
 
 }
 
+// animates the header elements
 
+
+function headingAnim() {
+  gsap.from("#top", {opacity: 0, x: "-50vw", duration: 2})
+}
+
+
+// carousel logic
+
+const imgList = ["./assets/img/Beach 2.png", "./assets/img/Beach.png", "./assets/img/Bees Napping.jpg"]
+
+function imgCarousel() {
+
+  
+  // let tl = gsap.timeline({repeat: -1})
+  // tl.fromTo("#carousel-img", {src: imgList[0], opacity: 0}, {opacity: 1, duration: 1})
+  // .fromTo("#carousel-img", {src: imgList[0], opacity: 1}, {opacity: 0, duration: 1})
+  // .fromTo("#carousel-img", {src: imgList[1], opacity: 0}, {opacity: 1, duration: 1})
+  // .fromTo("#carousel-img", {src: imgList[1], opacity: 1}, {opacity: 0, duration: 1})
+  // .fromTo("#carousel-img", {src: imgList[2], opacity: 0}, {opacity: 1, duration: 1})
+  // .to("#carousel-img", {src: imgList[0], opacity: 0, duration: 1})
+ 
+
+
+   
+
+}
